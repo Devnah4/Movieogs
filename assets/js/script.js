3decf8901101cf4ca05747c1da0bb184d73f5c8f
binhta <binhta102@gmail.com>
2022-03-30 23:07:20 -0400

    set layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,40 @@
-// Global Variables
+var searchBtn = document.querySelector("#searchButton")
+var moviePhoto = document.querySelector("#image")
+var titleBox = document.querySelector("#title")
+var descriptionBox = document.querySelector("#description")
 
-// Click button and it pulls the data needed
-// Displays the selected info on the page
-// applys the link to bestbuy
+
+searchBtn.addEventListener("click", function (){
+    var userInput = "bill and ted";
+    var requestUrl =
+    "https://imdb-api.com/en/API/SearchTitle/k_a7if4qo5/" + userInput;
+
+    fetch(requestUrl)
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        var results  = data.results;
+        console.log(results);
+
+        var tilteUrl = document.createElement('h2');
+        tilteUrl.textContent = results[0].title;
+        console.log(tilteUrl)
+        titleBox.append(tilteUrl);
+
+
+        var descriptionUrl = document.createElement('p')
+        descriptionUrl.textContent = results[0].description
+        console.log(descriptionUrl)
+        descriptionBox.append(descriptionUrl)
+        
+        var image = document.createElement('img')
+            image.src = results[0].image
+            document.getElementById('image').append(image);
+            
+    });
+
+    console.log(searchBtn)
+})
+
+
