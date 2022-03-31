5352c413b94303fd0bfaa3bcd0d417418ea73365
binhta <binhta102@gmail.com>
2022-03-31 19:52:42 -0400

    broken for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,14 @@ var searchBtn = document.querySelector("#searchButton")
 var moviePhoto = document.querySelector("#image")
 var titleBox = document.querySelector("#title")
 var descriptionBox = document.querySelector("#description")
+var searchMovie = document.querySelector("#userSearch")
 
 
 searchBtn.addEventListener("click", function (){
-    var userInput = "bill and ted";
+    var userInput = searchMovie.value
+    console.log(userInput)
     var requestUrl =
-    "https://imdb-api.com/en/API/SearchTitle/k_a7if4qo5/" + userInput;
+    "https://imdb-api.com/en/API/SearchTitle/k_2zr46be6/" + userInput;
 
     fetch(requestUrl)
     .then(function (response) {
@@ -15,23 +17,28 @@ searchBtn.addEventListener("click", function (){
     })
     .then(function (data) {
         var results  = data.results;
+        debugger
+        for (var i = 0; i < data.length; i++){
+        
         console.log(results);
 
         var tilteUrl = document.createElement('h2');
-        tilteUrl.textContent = results[0].title;
-        console.log(tilteUrl)
+        var descriptionUrl = document.createElement('p')
+
+        descriptionUrl.textContent = results[i].description
+        tilteUrl.textContent = results[i].title;
+
+        descriptionBox.append(descriptionUrl)
         titleBox.append(tilteUrl);
 
-
-        var descriptionUrl = document.createElement('p')
-        descriptionUrl.textContent = results[0].description
+        console.log(tilteUrl)
         console.log(descriptionUrl)
-        descriptionBox.append(descriptionUrl)
         
-        var image = document.createElement('img')
-            image.src = results[0].image
-            document.getElementById('image').append(image);
-            
+        
+        // var image = document.createElement('img')
+        // image.src = results[0].image
+        // document.getElementById('image').append(image);
+    }        
     });
 
     console.log(searchBtn)
