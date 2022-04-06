42b85d67d33ff6bf069cdcc9a474c034749e5638
Dev.Nah <mcthundercat99@gmail.com>
2022-04-05 21:11:54 -0400

    Basic code and image resources

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,52 +1,36 @@
 var searchBtn = document.querySelector("#searchButton")
-// var moviePhoto = document.querySelector("#image")
-// var titleBox = document.querySelector("#title")
-// var descriptionBox = document.querySelector("#description")
 var searchMovie = document.querySelector("#userSearch")
 var infoBox = document.querySelector('#infoBox')
 
-searchBtn.addEventListener("click", function (){
+searchBtn.addEventListener("click", function () {
     var userInput = searchMovie.value
-    console.log(userInput)
-    var requestUrl =
-    "https://imdb-api.com/en/API/SearchTitle/k_2zr46be6/" + userInput;
+    var requestUrl = "https://imdb-api.com/en/API/SearchTitle/k_2zr46be6/" + userInput;
 
     fetch(requestUrl)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
-        var results  = data.results;
-        //console.log(JSON.stringify(data)) 
-        console.log(data)
-
-
-        for (var i = 0; i < results.length; i++){
+        var results  = data.results; 
+        console.log(data);
+        for (var i = 0; i < 6; i++) {
         
-            console.log(results);
-
-            
             var tilteUrl = document.createElement('h2');
             var descriptionUrl = document.createElement('p')
             var image = document.createElement('img')
 
-            descriptionUrl.textContent = results[i].description
+            descriptionUrl.textContent = results[i].description;
+            descriptionUrl.className = 'description';
             tilteUrl.textContent = results[i].title;
+            tilteUrl.className = 'titles';
+            image.src = results[i].image;
+            image.className = 'image-infoBox';
+            image.style.width = "200px";
+            image.style.height = "350px";
 
-            image.src = results[i].image
-
-            infoBox.appendChild(descriptionUrl)
+            infoBox.appendChild(image);
             infoBox.appendChild(tilteUrl);
-            infoBox.appendChild(image)
-            
-            // console.log(tilteUrl)
-            // console.log(descriptionUrl)
-                
-            //document.getElementById('image').append(image);
-    }        
+            infoBox.appendChild(descriptionUrl);
+        }        
     });
-
-    console.log(searchBtn)
 })
-
-// testing push -khai hello 
