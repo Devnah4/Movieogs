5a83661875aa1166214044b313cf3b608ae3abf7
Khai Truong <kminht@gmail.com>
2022-04-07 20:55:20 -0400

    back to display movie only

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,103 +1,100 @@
-var searchBtn = document.querySelector("#searchButton")
+var searchBtn = document.querySelector("#searchButton");
 // var moviePhoto = document.querySelector("#image")
 // var titleBox = document.querySelector("#title")
 // var descriptionBox = document.querySelector("#description")
-var searchMovie = document.querySelector("#userSearch")
-var infoBox = document.querySelector('#infoBox')
-
+var searchMovie = document.querySelector("#userSearch");
+var infoBox = document.querySelector("#infoBox");
 
 // gets user input, passes to fetch api function
-searchBtn.addEventListener("click", function (){
-    var userInput = searchMovie.value
-    console.log(userInput)
+searchBtn.addEventListener("click", function () {
+  var userInput = searchMovie.value;
+  console.log(userInput);
 
-    //reload page
-    //location.reload();
+  //reload page
+  //location.reload();
 
-    //call api fun
-    fetchApi(userInput);
-
-})
-
+  //call api fun
+  fetchApi(userInput);
+});
 
 // will get api's and call the right functions within
-function fetchApi(userInput){
+function fetchApi(userInput) {
+  console.log("inside fetch api function");
 
-    console.log("inside fetch api function");
-
-    var requestUrl =
+  var requestUrl =
     "https://imdb-api.com/en/API/SearchTitle/k_2zr46be6/" + userInput;
 
-    fetch(requestUrl)
+  fetch(requestUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
-        console.log(data) // testing
-        var results  = data.results;
-        displaySearch(results);
+      console.log(data); // testing
+      var results = data.results;
+      displaySearch(results);
 
-        // do second fetch for youtube trailer
-
+      // do second fetch for youtube trailer
     });
-
 }
 
+// display
+function displaySearch(results) {
+  console.log("inside display search function");
+  console.log(results);
 
-// display 
-function displaySearch(results){
-    console.log("inside display search function");
-    console.log(results);
+  for (var i = 0; i < 6; i++) {
+    var movieItem = {
+      title: results[i].title,
+      description: results[i].description,
+      image: results[i].image,
+      movieId: results[i].id,
+    };
+
+  // display info to page 
+  document.getElementById(
+    "infoBox-" + i
+  ).innerHTML = `<div class ="card" style="width: 18rem;">
+              <div class ="card-body">
+                  <img class ="card-img-top" src=${movieItem.image}></img>
+                  <h5 class ="card-title">${movieItem.title}</h5>
+                  <p class ="card-text">${movieItem.description}</p>
+              </div>
+          </div>`;
+
+   // getTrailer(i, movieItem.movieId);
 
 
-    for (var i = 0; i < 6; i++){
-        
-
-        var movieItem ={
-            title: results[i].title,
-            description: results[i].description,
-            image: results[i].image,
-            movieId: results[i].id,
-        };   
-
-        // currenty does not get url passed back from function.
-        var video = getTrailer(movieItem.movieId);
-        console.log(" video url in loop " + video);
-
-      
-        document.getElementById("infoBox-" + i ).innerHTML = 
-            `<div class ="card" style="width: 18rem;">
-                <div class ="card-body">
-                    <img class ="card-img-top" src=${movieItem.image}></img>
-                    <h5 class ="card-title">${movieItem.title}</h5>
-                    <p class ="card-text">${movieItem.description}</p>
-                </div>
-            </div>`;
- 
-}  
-
-}; // end displaySearch
+  }
+} // end displaySearch
 
 // get trailer link by fetching from api
 // needs which array to look for when passing in the data
-function getTrailer(movieId){
+// function getTrailer(i, movieId) {
+//   var requestUrl2 =
+//     "https://imdb-api.com/en/API/YouTubeTrailer/k_a7if4qo5/" + movieId;
+//   console.log(requestUrl2);
 
-    var requestUrl2 = "https://imdb-api.com/en/API/YouTubeTrailer/k_a7if4qo5/" + movieId;
+//   fetch(requestUrl2).then(function (response) {
+//     response.json().then(function (data) {
+//       console.log(data);
 
-    fetch(requestUrl2)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        
-        var video = data.videoUrl 
-        console.log(video);
-        
+//       console.log(data.videoId);
+//     // pass i and videoId to display function
+//       displayTrailer(i,data.videoId);
 
-    });
-
+//     });
+//   });
+  
+// }
 
 
-}
+// function displayTrailer(i,id){
 
-// getTrailer("tt1375666");
+//  var video = id;
+
+//     document.getElementById("infoVideo-" + i ).innerHTML =`
+//         <iframe class ="card-img-bottom width="350" height="300" src="https://www.youtube.com/embed/${video}" title="YouTube video player" frameborder="0"</iframe>
+//         `
+// }
+
+
