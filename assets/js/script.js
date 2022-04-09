0db6810f73cbe2bedf990c3a15411dc074ccaf1b
Khai Truong <kminht@gmail.com>
2022-04-09 11:53:56 -0400

    book api works and able to display info, added error catch to api fetches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,34 @@
 var searchBtn = document.querySelector("#searchButton");
 var searchBook = document.querySelector("#searchBook");
-// var moviePhoto = document.querySelector("#image")
-// var titleBox = document.querySelector("#title")
-// var descriptionBox = document.querySelector("#description")
 var searchMovie = document.querySelector("#userSearch");
 var infoBox = document.querySelector("#infoBox");
 var googleApi = 'AIzaSyAH7Ftu1pU975MStP0M3ev6DEiGQqSkzF0';
+var imdbApi = 'k_2zr46be6';
 
 // gets user movie input, passes to fetch api function
 searchBtn.addEventListener("click", function () {
   var userInput = searchMovie.value;
-  console.log(userInput);
-
+ // infoBox.innerHTML = '';
   //call movie api function
   fetchApi(userInput);
 });
 
 searchBook.addEventListener("click",function(){
     var userInput = searchMovie.value;
+   // infoBox.innerHTML = '';
+    // call book api function 
+    fetchBookApi(userInput);
 
 })
 
 
 // will get api's and call the right functions within
 function fetchApi(userInput) {
-  console.log("inside fetch api function");
+    // clear old document
+   
 // hide key later
   var requestUrl =
-    "https://imdb-api.com/en/API/SearchTitle/k_2zr46be6/" + userInput;
+    `https://imdb-api.com/en/API/SearchTitle/${imdbApi}/${userInput}`;
 
   fetch(requestUrl)
     .then(function (response) {
@@ -37,8 +38,11 @@ function fetchApi(userInput) {
       console.log(data); // testing
       var results = data.results;
       displaySearch(results);
-
+        
       // do second fetch for youtube trailer
+    })
+    .catch(function(error){
+        alert("Error. Please enter a valid search")
     });
 }
 
@@ -72,9 +76,9 @@ function displaySearch(results) {
 
 
 function fetchBookApi(userInput){
-    infoBox.innerHTML = '';
+    
     console.log(searchMovie.value);
-    fetch(`https://www.googleapis.com/books/v1/volumes?q=${bookInput}:keyes&key=${googleApi}`)
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${userInput}:keyes&key=${googleApi}`)
     .then(function (response) {
         return response.json();
     })
@@ -82,18 +86,21 @@ function fetchBookApi(userInput){
         var results = data.items;
         console.log(data);
        displayBook(results);
+    })
+    .catch(function(error){
+        alert("Error. Please enter a valid search")
     });
 };
 
-
+// fix items
 function displayBook(results){
-    
+
     for (var i = 0; i < 6; i++) {
-        var movieItem = {
-          title: results[i].title,
-          description: results[i].description,
-          image: results[i].image,
-          link: results[i].id,
+        var bookItem = {
+          title: results[i].volumeInfo.title,
+          description: results[i].volumeInfo.description,
+          image: results[i].volumeInfo.imageLinks.thumbnail,
+          link: results[i].saleInfo.buyLink,
         };
     
       // display info to page 
@@ -101,10 +108,10 @@ function displayBook(results){
         "infoBox-" + i
       ).innerHTML = `<div class ="card" style="width: 18rem;">
                   <div class ="card-body">
-                      <img class ="card-img-top" src=${movieItem.image}></img>
-                      <h6 class ="card-title">${movieItem.title}</h5>
-                      <p class ="card-text">${movieItem.description}</p>
-                      <a href ="https://www.imdb.com/title/${movieItem.link}" class="card-link" target ="_blank">Click here for more info!</a>
+                      <img class ="card-img-top" src=${bookItem.image}></img>
+                      <h6 class ="card-title">${bookItem.title}</h5>
+                      <p class ="card-text">${bookItem.description}</p>
+                      <a href ="${bookItem.link}" class="card-link" target ="_blank">Click here for more info!</a>
                   </div>
               </div>`;
     
